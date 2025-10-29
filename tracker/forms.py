d8a9ad55032ad7339622b545c83811b1d320e319
thealisaeedi <thealisaeedi@gmail.com>
2025-10-29 22:56:27 +0330

    Trackers: Category added to forms

---
+++ tracker/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Transaction, Category
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount', 'type', 'description', 'category']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['category'].queryset = Category.objects.filter(user=user)
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('IN', 'Income'),
+        ('OUT', 'Expense'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=3)
+    type = models.CharField(max_length=3, choices=TRANSACTION_TYPES)
+    description = models.CharField(max_length=300, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount}"
