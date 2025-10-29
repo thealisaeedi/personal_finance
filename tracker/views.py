6d89347e1412b8ced689b792fdd1363c1693a83e
thealisaeedi <thealisaeedi@gmail.com>
2025-10-29 22:56:27 +0330

    Trackers: Category added to forms

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Transaction, Category
+from .forms import TransactionForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def add_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, user=request.user)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm(user=request.user)
+    return render(request, 'tracker/add_transaction.html', {'form': form})
+
+@login_required
+def transaction_list(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    return render(request, 'tracker/transaction_list.html', {'transactions': transactions})
+
