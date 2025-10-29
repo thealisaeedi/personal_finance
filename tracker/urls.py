14ae3e88d700f79e468c14d52178c6720816c3f1
thealisaeedi <thealisaeedi@gmail.com>
2025-10-29 23:41:15 +0330

    sum of income and expense, added, and it can be fitltered

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('add/', views.add_transaction, name='add_transaction'),
     path('list/', views.transaction_list, name='transaction_list'),
+    path('report/', views.report, name='report'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -2,6 +2,35 @@ from django.shortcuts import render, redirect
 from .models import Transaction, Category
 from .forms import TransactionForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
+
+
+@login_required
+def report(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    total_income = transactions.filter(type='IN').aggregate(Sum('amount'))['amount__sum'] or 0
+    total_expense = transactions.filter(type='OUT').aggregate(Sum('amount'))['amount__sum'] or 0
+    balance = total_income - total_expense
+
+    category_id = request.GET.get('category')
+    type_filter = request.GET.get('type')
+    
+    if category_id:
+        transactions = transactions.filter(category__id=category_id)
+    if type_filter:
+        transactions = transactions.filter(type=type_filter)
+
+    categories = Category.objects.filter(user=request.user)
+
+    context = {
+        'transactions': transactions,
+        'total_income': total_income,
+        'total_expense': total_expense,
+        'balance': balance,
+        'categories': categories,
+    }
+
+    return render(request, 'tracker/report.html', context)
 
 @login_required
 def add_transaction(request):
